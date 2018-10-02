bcd68b0153367267ab6143eb9ac6e47e8a59d101
Xenobyte42 <andmicha2011@bk.ru>
2018-10-02 16:33:45 +0300

    Compited pylint, some if-else fixes

--- grep.py
+++ grep.py
@@ -1,22 +1,46 @@
+"""
+The module implements the grep function as well as some of its keys:
+  -h, --help         show this help message and exit
+  -v                 Selected lines are those not matching pattern.
+  -i                 Perform case insensitive matching.
+  -c                 Only a count of selected lines is written to standard
+                     output.
+  -n                 Each output line is preceded by its relative line number
+                     in the file, starting at line 1.
+  -C CONTEXT         Print num lines of leading and trailing context
+                     surrounding each match.
+  -B BEFORE_CONTEXT  Print num lines of trailing context after each match
+  -A AFTER_CONTEXT   Print num lines of leading context before each match.
+"""
+
 
 import argparse
 import sys
-import re
 
 
 def clean_line(line, params):
+    """The function removes minor spaces and \n
+    from the string, and lowercase if required
+    """
+
     cleaned_line = line.rstrip().rstrip('\n')
     if params.ignore_case:
         cleaned_line = cleaned_line.lower()
     return cleaned_line
 
 def append_match(match, matches):
+    """Adds a string if it is not in the list"""
+
     if match not in matches:
         matches.append(match)
 
 def add_lines(matches, index, lines, params):
-    start_idx = 0
-    last_idx = 0
+    """Adds the appropriate strings to the array,
+    as well as the strings around them, if necessary
+    """
+
+    start_idx = index
+    last_idx = index
     if params.context > 0:
         start_idx = index - params.context
         if start_idx < 0:
@@ -25,7 +49,6 @@ def add_lines(matches, index, lines, params):
         if last_idx >= len(lines):
             last_idx = len(lines) - 1
     elif params.after_context > 0:
-        start_idx = index
         last_idx = index + params.after_context
         if last_idx >= len(lines):
             last_idx = len(lines) - 1
@@ -33,18 +56,17 @@ def add_lines(matches, index, lines, params):
         start_idx = index - params.before_context
         if start_idx < 0:
             start_idx = 0
-        last_idx = index
-    else:
-        start_idx = index
-        last_idx = index
+
     for i in range(start_idx, last_idx + 1):
-            lines[i] = lines[i].rstrip('\n')
-            append_match([i + 1, lines[i]], matches)
+        lines[i] = lines[i].rstrip('\n')
+        append_match([i + 1, lines[i]], matches)
 
 def find_pattern(pattern, line):
+    """Recursively (just for *) looks for a pattern in the input string"""
+
     for l_idx in range(0, len(line)):
         temp_idx = l_idx
-        for p_idx in range(0, len(pattern)):
+        for p_idx, _ in enumerate(pattern):
             if temp_idx == len(line):
                 break
 
@@ -59,12 +81,14 @@ def find_pattern(pattern, line):
                     temp_idx += 1
                 else:
                     break
-    
+
             if p_idx == len(pattern) - 1:
                 return True
     return False
 
 def parse_lines(lines, params):
+    """Creates a list of required strings that match the pattern and parameters"""
+
     matches = []
     if params.ignore_case:
         params.pattern = params.pattern.lower()
@@ -73,51 +97,54 @@ def parse_lines(lines, params):
         line = line.rstrip('\n')
         cleaned_line = clean_line(line, params)
 
-        if params.invert:
-            if find_pattern(params.pattern, cleaned_line) is False:
-                add_lines(matches, index, lines, params)
-        else:
-            if find_pattern(params.pattern, cleaned_line) is True:
-                add_lines(matches, index, lines, params)
+        if params.invert ^ find_pattern(params.pattern, cleaned_line):
+            add_lines(matches, index, lines, params)
+
     return matches
 
 def output(line):
+    """Unittest output function"""
+
     print(line)
 
 def output_matches(matches, params):
-    if params.count:
-        output(str(len(matches)))
-    else:
+    """The correct output information, in accordance with the parameters"""
+
+    if not params.count:
         if params.context or params.after_context or params.before_context:
-            last_idx = -2
+            last_idx = -2   # Just for first '--' in block output
             for match in matches:
                 if match[0] - last_idx > 1:
                     print("--")
                 last_idx = match[0]
                 if params.line_number:
                     print(match[0], '-', sep="", end="")
-                    output(match[1])
-                else:
-                    output(match[1])
+                output(match[1])
             print("--")
         else:
             for match in matches:
                 if params.line_number:
                     print(match[0], ':', sep="", end="")
-                    output(match[1])
-                else:
-                    output(match[1])
+                output(match[1])
+    else:
+        output(str(len(matches)))
 
 def grep(lines, params):
+    """UNIX grep realization"""
+
     matches = parse_lines(lines, params)
     output_matches(matches, params)
 
 def parse_args(args):
+    """Сommand line argument parser"""
+
     parser = argparse.ArgumentParser(description='This is a simple grep on python')
     parser.add_argument(
-        '-v', action="store_true", dest="invert", default=False, help='Selected lines are those not matching pattern.')
+        '-v', action="store_true", dest="invert", default=False,
+        help='Selected lines are those not matching pattern.')
     parser.add_argument(
-        '-i', action="store_true", dest="ignore_case", default=False, help='Perform case insensitive matching.')
+        '-i', action="store_true", dest="ignore_case", default=False,
+        help='Perform case insensitive matching.')
     parser.add_argument(
         '-c',
         action="store_true",
@@ -129,7 +156,8 @@ def parse_args(args):
         action="store_true",
         dest="line_number",
         default=False,
-        help='Each output line is preceded by its relative line number in the file, starting at line 1.')
+        help='Each output line is preceded by its relative line number \
+             in the file, starting at line 1.')
     parser.add_argument(
         '-C',
         action="store",
@@ -151,11 +179,13 @@ def parse_args(args):
         type=int,
         default=0,
         help='Print num lines of leading context before each match.')
-    parser.add_argument('pattern', action="store", help='Search pattern. Can contain magic symbols: ?*')
+    parser.add_argument('pattern', action="store",
+                        help='Search pattern. Can contain magic symbols: ?*')
     return parser.parse_args(args)
 
 
 def main():
+    """Main function, calls the argument parsing and processing"""
     params = parse_args(sys.argv[1:])
     grep(sys.stdin.readlines(), params)
 
